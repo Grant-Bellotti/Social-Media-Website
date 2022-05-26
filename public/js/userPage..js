29b36efcb37ed4d6fd170f32efa6746ac305d980
AngelNBazan <angelbazan150@gmail.com>
2022-05-26 11:41:54 -0700

    userPage.js
    123

---
+++ public/js/userPage..js
@@ -0,0 +1,126 @@
+let ident;
+let messageid;
+let socket = io();
+//socket.emit('updateComments', {'text': text,'messageID':id,'user': user});
+let postColor;
+
+function commentit(id){
+  let text = $("#commentBox").val();
+  if(text != "") {
+    $.get("/checkAuthenticated",function(info){
+      if(info.error) {
+        alert(info.message);
+        return false;
+      } else {
+        let user = info.user;
+         $.ajax({
+              url: "/getData",
+              type: "GET",
+             data: {messageID:messageid},
+              success: function(data){
+                let oldComment = data.comments;
+                 postColor = data.color
+                      $.ajax({
+                        url: "/storeComment",
+                        type: "POST",
+                       data: {text:text,messageID:messageid,user:user,oldComment:oldComment,color:data.color},
+                        success: function(data2){
+
+                        } ,
+                        dataType: "json"
+                      });
+              } ,
+              dataType: "json"
+            });
+
+       socket.emit("updateComments",{"text":text,"messageID":messageid,"user":user})
+       $("#commentBox").val("");
+      }
+    });
+  }
+
+}
+
+  
+  $(document).ready(function(){ 
+    $.ajax({
+      url: "/getPostID",
+      type: "GET",
+      data: {},
+      success: function(data){
+        messageid = data.ID
+        socket.on('welcome',function(){
+          $.ajax({
+            url: "/getData",
+            type: "GET",
+            data: {messageID:messageid},
+            success: function(data2){
+              postColor = data2.color
+              let newComment =""
+              if(data2.type == 'Text'){
+                newComment = 
+                `<div class="userBlock" style="background-color:${postColor}">
+                <h1>
+                ${data2.message}
+                </h1>
+                <p>
+                ${data2.realMessage}
+                </p>
+                </div>
+                <div>
+                Comments
+                
+                ${data2.comments}
+                </div>
+                `
+              }
+              else {
+                newComment = 
+                `<div class="userBlock" style="background-color:${postColor}">
+                User:${data2.user}
+                <img
+                style="background-color:${postColor}"
+                src="images/${data2.message}"
+                >
+                </div>
+                <div>
+                Comments
+                
+                ${data2.comments}
+                </div>
+                `
+              }
+
+
+              $("#messages").append(
+                newComment
+                )              
+              } ,
+            dataType: "json"
+          });
+        })
+      } ,
+      dataType: "json"
+    });
+
+
+      
+    
+//Get message from server.
+
+    
+    socket.on('updateComments', function(data) {
+      console.log(data,"updateComments");
+        if(messageid==data.messageID){
+          console.log(data,'true message check');
+          $("#messages").append(
+            `<div>
+            <p class="commentBlock" style="background-color:${postColor}">
+            ${data.user}: 
+            ${data.text}
+            </p>
+            <div>
+            `)
+        }
+      });
+  });
